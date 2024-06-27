0d11524ce1b8fdbe72bd8e5b5500a5c0b28c929f
Abhi2003-h <119411036+Abhi2003-h@users.noreply.github.com>
2024-06-27 18:55:19 +0530

    Update desktop.py

--- business_theme_v14/config/desktop.py
+++ business_theme_v14/config/desktop.py
@@ -4,7 +4,7 @@ def get_data():
 	return [
 		{
 			"module_name": "Business Theme V14",
-			"color": "#E6E6FA",
+			"color": "grey",
 			"icon": "octicon octicon-file-directory",
 			"type": "module",
 			"label": _("Business Theme V14")
